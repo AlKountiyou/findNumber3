b09beca9e8d81a81f1dd74e74f10419e07ea43ce
AlKountiyou <64207917+AlKountiyou@users.noreply.github.com>
2022-11-18 17:49:09 +0000

    Add files via upload

---
+++ findNumber3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+int number, inf, sup;
+Random rnd = new Random();
+ArrayList choix = new ArrayList();
+int i = 0;
+entrerBornes:
+Console.WriteLine("Entrer la borne inférieure:");
+string stringInf = Console.ReadLine();
+Console.WriteLine("Entrer la borne supérieure:");
+string stringSup = Console.ReadLine();
+if (!int.TryParse(stringInf, out inf) || !int.TryParse(stringSup, out sup))
+{
+    goto entrerBornes;
+}
+int nbreMystere = rnd.Next(inf, sup + 1);
+entrerNombre:
+Console.WriteLine("Entrer un nombre compris entre " + inf + " et " + sup + ":");
+string stringNumber = Console.ReadLine();
+if (!int.TryParse(stringNumber, out number))
+{
+    choix.Add(stringNumber);
+    goto entrerNombre;
+}
+else
+{
+    if (number < inf || number > sup)
+    {
+        choix.Add(number);
+        goto entrerNombre;
+    }
+}
+if (number == nbreMystere)
+{
+    Console.WriteLine("Vous avez gagné");
+    Console.WriteLine("Tentatives: ");
+    foreach (var nbr in choix)
+    {
+        Console.Write(nbr + " ");
+    }
+}
+else
+{
+    Console.WriteLine("vous avez perdu!");
+    choix.Add(number);
+    goto entrerNombre;
+}
+
